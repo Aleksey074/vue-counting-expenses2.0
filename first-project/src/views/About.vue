e3e99f01a44909612eacca7b8abd223703d7d720
Aleksey074 <94966508+Aleksey074@users.noreply.github.com>
2022-02-02 19:11:51 +0500

    версия от 2.02
    Добавил горячие кнопки

---
+++ first-project/src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="about">
+    <h1>Самые частые расходы</h1>
+    <hr>
+    <div class="main-btns">
+    <button class="btn-fast-pay"><router-link class="link-fast-pay" to="/add/payment/Еда?value=200">Еда 200 рублей</router-link></button>
+    <button class="btn-fast-pay"><router-link class="link-fast-pay" to="/add/payment/Транспорт?value=50">Транспорт 50 рублей</router-link></button>
+    <button class="btn-fast-pay"><router-link class="link-fast-pay"  to="/add/payment/Развлечения?value=2000">Развлечения 2000 рублей</router-link></button>
+    </div>
+  </div>
+</template>
+
+
+<style scoped>
+.main-btns{
+  display: flex;
+  justify-content: center;
+  margin-top: 40px;
+}
+.btn-fast-pay{
+  height: 50px;
+  margin-right: 30px;
+}
+.link-fast-pay{
+  text-decoration: none;
+  font-size: 40px;
+}
+</style>
